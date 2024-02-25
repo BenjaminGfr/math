63187d5c2d66d085b83ff3d94698832eae2bedd7
Benjamin <benjamin.gfroerer@gmx.de>
2024-02-25 21:05:11 +0100

    Updated to make the task clear

--- scene.gd
+++ scene.gd
@@ -3,4 +3,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("2 + 2 = ", 2+2)
+	print("2 - 2 = ")
+	print("2 * 2 = ")
+	print("2 / 2 = ")
 	
